811a73c9c25272ea8b72e953b5df9a9714ddede4
Björn Bebensee <bjoern.bebensee@rwth-aachen.de>
2018-10-09 04:59:24 +0200

    Splitting dataset in test and validation set

--- PUBG/learn.py
+++ PUBG/learn.py
@@ -6,6 +6,7 @@ from fastai.structured import *
 
 # Import our models and metrics
 from sklearn.ensemble import RandomForestRegressor
+from sklearn import model_selection
 from sklearn import metrics
 from os import makedirs
 
@@ -18,10 +19,18 @@ df_raw = pd.read_csv(f'{PATH}train.csv')
 
 # Split dataset
 print("Splitting dataset...")
-
+df_trn, y_trn, nas = proc_df(df_raw, 'winPlacePerc')
+X_train, X_valid, y_train, y_valid = model_selection.train_test_split(df_trn, y_trn)
+set_rf_samples(20000)
 
 # Train model
 print("Training...")
 m = RandomForestRegressor(n_jobs=-1)
-m.fit(df_raw.drop('winPlacePerc', axis=1), df_raw.winPlacePerc)
-print(m.score(df_raw.drop('winPlacePerc', axis=1), df_raw.winPlacePerc))
+m.fit(X_train, y_train)
+print(m.score(X_train, y_train))
+print(m.score(X_valid, y_valid))
+
+preds = np.stack([t.predict(X_valid) for t in m.estimators_])
+print(preds[:,0], np.mean(preds[:,0]), y_valid[0])
+plt.plot([metrics.r2_score(y_valid, np.mean(preds[:i+1], axis=0)) for i in range(10)])
+plt.show()
