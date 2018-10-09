4e214dfbec57820aa7a27b573548405b40e1c36f
Björn Bebensee <bjoern.bebensee@rwth-aachen.de>
2018-10-09 05:19:49 +0200

    Predict a result from test set

--- PUBG/learn.py
+++ PUBG/learn.py
@@ -10,9 +10,10 @@ from sklearn import model_selection
 from sklearn import metrics
 from os import makedirs
 
-# Define path of our dataset
+# Define path of our dataset and load files
 PATH = "data/"
 df_raw = pd.read_csv(f'{PATH}train.csv')
+df_test = pd.read_csv(f'{PATH}test.csv')
 #os.makedirs('tmp', exist_ok=True)
 #df_raw.to_feather('tmp/pubg-raw')
 #df_raw = pd.read_feather('tmp/pubg-raw')
@@ -25,12 +26,13 @@ set_rf_samples(20000)
 
 # Train model
 print("Training...")
-m = RandomForestRegressor(n_jobs=-1)
+m = RandomForestRegressor(n_estimators=100, min_samples_leaf=3, max_features='sqrt', n_jobs=-1)
 m.fit(X_train, y_train)
 print(m.score(X_train, y_train))
 print(m.score(X_valid, y_valid))
 
-preds = np.stack([t.predict(X_valid) for t in m.estimators_])
-print(preds[:,0], np.mean(preds[:,0]), y_valid[0])
-plt.plot([metrics.r2_score(y_valid, np.mean(preds[:i+1], axis=0)) for i in range(10)])
-plt.show()
+# Use model to make our predictions
+print("Predicting...")
+df_test['winPlacePerc'] = m.predict(df_test)
+submit = df_test[['Id', 'winPlacePerc']]
+submit.to_csv('submit.csv', index=False)
