b05712b31c28d75b55cc66eee81b465f7f25c980
Björn Bebensee <bjoern.bebensee@rwth-aachen.de>
2018-10-09 04:24:03 +0200

    Initial commit

---
+++ PUBG/learn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Import fast.ai library
+from fastai.imports import *
+from fastai.structured import *
+
+# Import our models and metrics
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from os import makedirs
+
+# Define path of our dataset
+PATH = "data/"
+df_raw = pd.read_csv(f'{PATH}train.csv')
+
+# Split dataset
+print("Splitting dataset...")
+os.makedirs('tmp', exist_ok=True)
+df_raw.to_feather('tmp/pubg-raw')
+
+# Train model
+print("Training...")
+m = RandomForestRegressor(n_jobs=-1)
+m.fit(df_raw.drop('winPlacePerc', axis=1), df_raw.winPlacePerc)
+print(m.score(df_raw.drop('winPlacePerc', axis=1), df_raw.winPlacePerc))
